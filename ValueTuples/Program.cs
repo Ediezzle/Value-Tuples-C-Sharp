d5fe8e489ac7b506fc0c27a2d7cc8a357f7621fd
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 20:16:12 +0200

    first and final commit

---
+++ ValueTuples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValueTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //value tuples can include more than 8 values. unlike tuples
+            ValueTuple<int, string, string> mbada = (1, "Bill", "Gates");
+
+            (int Id, string FirstName, string LastName) person = (1, "Bill", "Gates");
+            Console.WriteLine(person.Id);
+
+            var bourgeosie = (Id: 1, FirstName: "Jeff", LastName: "Benzo");
+
+        }
+    }
+}
